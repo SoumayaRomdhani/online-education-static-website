9423e928ad1341ea634fba48426006c1ad69c3b7
Soumaya <157825319+SoumayaRomdhani@users.noreply.github.com>
2024-02-15 19:22:46 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+//change navbar style on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+
+//show hide faq answers 
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq =>{
+    faq.addEventListener('click',() => {
+        faq.classList.toggle('open');
+    })
+})
+
+
+
+
